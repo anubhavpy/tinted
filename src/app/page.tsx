65c56297b122a6e47f58aeb76a6581cdd7647d05
Oditha <oditha14@gmail.com>
2024-04-09 22:53:59 -0400

    idfk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+
+"use client"
+import { useEffect, useState, Suspense } from 'react';
+import ReactPlayer from 'react-player';
+
 export default function HomePage() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
+    <>
+      {isClient && (
+        <Suspense className="w-96 h-72">
+          <ReactPlayer url="/0209.mov" />
+        </Suspense>
+      )}
+    </>
   );
 }
+
